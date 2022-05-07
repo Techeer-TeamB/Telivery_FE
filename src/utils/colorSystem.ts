7c4e1be2834c7bb624e43c93c5f9495143626840
suaj <rmdwjdmind@gmail.com>
2022-05-07 21:18:28 +0900

    :zap: Add utils components file
    daa ColorSystem in utils

---
+++ src/utils/colorSystem.ts
@@ -0,0 +1,14 @@
+export const ColorSystem ={
+    mainColor: {
+        blue : "#2986C3",
+        pink : "#FD927D",
+        yellow: "#F7C560",
+        ivory: "#FFF6E6",
+        footer: "#3E454C",
+    },
+
+    textColor: {
+        mainText: "#333333",
+        subText: "#656565",
+    }
+}
