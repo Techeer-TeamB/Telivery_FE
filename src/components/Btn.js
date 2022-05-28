7d2a01a2806228733492b55563d66d2f568ab49b
suaj <rmdwjdmind@gmail.com>
2022-05-28 21:58:23 +0900

    :zap: publishing login page

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 
-const Btn = () => {
-  return <></>;
+const Btn = (props) => {
+  const Container = styled.div`
+    width: 30vw;
+    height: 40px;
+
+    border-radius: 26px;
+    background-color: #2986c3;
+    color: #fbfbfb;
+  `;
+
+  return (
+    <Container className="text-center leading-9 mx-auto mt-8">
+      {props.Text}
+    </Container>
+  );
 };
 
 export default Btn;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import InputBox from "@components/inputBox";
+import Btn from "@components/Btn";
 import styled from "styled-components";
 import loginBackground from "../images/loginBackground.png";
 
@@ -22,12 +23,20 @@ export const Login = () => {
     box-shadow: 2px 2px 8px #e2e2e2;
   `;
 
+  const AccountFinder = styled.p`
+    color: #fd927d;
+  `;
+
   return (
     <MainCon className="fixed">
       <LoginCon className="m-auto flex flex-col mt-36 text-center">
         <h1 className="text-xl font-bold mb-6 mt-12">WELCOME!</h1>
         <InputBox className="pb-4" Type="ID" />
         <InputBox className="mt-4" Type="PASSWORD" />
+        <Btn Text="Login" className="mt-2" />
+        <AccountFinder className="text-xs font-bold mt-1">
+          Don’t have an account?
+        </AccountFinder>
       </LoginCon>
     </MainCon>
   );
